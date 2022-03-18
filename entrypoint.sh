ae5c62360bc59aa94212cbc3c391da85142ff75c
IsraelMiles <israelmiles@microsoft.com>
2022-03-18 15:58:52 -0700

    reorder args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export ADO_PROJECT=$1
-export GH_REPO=$2
-export ISSUE_NUM=$3
-export ADO_TOKEN=$4
-export GITHUB_TOKEN=$5
-export ADO_URI=$6
+export ADO_URI=$1
+export ADO_PROJECT=$2
+export GH_REPO=$3
+export ISSUE_NUM=$4
+export ADO_TOKEN=$5
+export GITHUB_TOKEN=$6
 
-dotnet /gh-sync.dll pull-gh --dry-run $GH_REPO $ISSUE_NUM
+dotnet /gh-sync.dll pull-gh $GH_REPO $ISSUE_NUM --dry-run
